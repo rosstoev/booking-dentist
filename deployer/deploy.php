cd23afd2ec3f7eb35aa6d342b16c78e2ccdc5991
Rosen Stoev <rossto3v@gmail.com>
2024-12-28 16:39:57 +0000

    implement deploy

--- deployer/deploy.php
+++ deployer/deploy.php
@@ -6,7 +6,9 @@ require 'recipe/symfony.php';
 // Config
 
 set('repository', 'git@github.com:rosstoev/booking-dentist.git');
-//set('branch', 'master');
+
+// Number of releases to preserve in releases folder.
+set('keep_releases', 3);
 
 add('shared_files', []);
 add('shared_dirs', []);
@@ -14,10 +16,19 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('127.0.0.1')
+host('demo')
+    ->setHostname('192.168.0.106')
+    ->set('branch', 'develop')
     ->set('remote_user', 'deployer')
-    ->set('deploy_path', '/var/www/booking-dentist');
+    ->set('deploy_path', '/var/www/bookingdentist');
+
+// Tasks
+task('database:create', function () {
+    run("{{bin/console}} doctrine:database:create --if-not-exists");
+});
 
 // Hooks
-
+before('database:migrate', 'database:create');
+after('deploy:symlink', 'database:migrate');
 after('deploy:failed', 'deploy:unlock');
+
